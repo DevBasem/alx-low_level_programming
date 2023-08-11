96e98bff7021ca0aa8be46c06f3f7d186c0ad849
DevBasem <engr.basemahmed@gmail.com>
2023-08-11 07:52:12 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * this program prints single digit numbers
+ */
+int main(void)
+{
+int x = 48;
+while (x <= 57)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
