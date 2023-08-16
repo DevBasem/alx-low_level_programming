2b9180dd7fb947190844497700042692f9d1d4a5
DevBasem <engr.basemahmed@gmail.com>
2023-08-15 19:20:06 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * nLength - checks the length of the number.
+ * @n: The number to check
+ *
+ * Return: digits number
+ */
+int nLength(int n)
+{
+int length = 0;
+if (!n)
+return (1);
+while (n)
+{
+n = n / 10;
+length += 1;
+}
+return (length);
+}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c, i10s;
+unsigned long f1 = 1, f2 = 2, sum, max = 100000000, f1o = 0, sumo = 0;
+for (c = 1; c <= 98; c++)
+{
+if (f1o > 0)
+printf("%lu", f1o);
+i10s = nLength(max) - 1 - nLength(f1);
+while (f1o > 0 && i10s > 0)
+{
+printf("%d", 0);
+i10s--;
+}
+printf("%lu", f1);
+sum = (f1 + f2) % max;
+f1 = f2;
+f1o = f2o;
+f2 = sum;
+f2o = sumo;
+if (c != 98)
+printf(", ");
+else
+print("\n");
+}
+return (0);
+}
