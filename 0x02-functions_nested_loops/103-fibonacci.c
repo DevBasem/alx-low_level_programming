e1c322b38f8c4d4cc5b433229ad521b16d85fcfe
DevBasem <engr.basemahmed@gmail.com>
2023-08-15 18:53:49 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+float tS;
+unsigned long fib1 = 0, fib2 = 1, sum;
+while (1)
+{
+sum = fib1 + fib2;
+if (sum > 4000000)
+break;
+if ((sum % 2) == 0)
+tS += sum;
+fib1 = fib2;
+fib2 = sum;
+}
+printf("%.0f\n", total_sum);
+return (0);
+}
