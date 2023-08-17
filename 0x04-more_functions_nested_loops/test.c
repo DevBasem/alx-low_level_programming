081211a59d7d225593151588b2bda94cc1f649a9
DevBasem <engr.basemahmed@gmail.com>
2023-08-17 16:49:00 -0700

    12. Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n : the number to print.
+ *
+ * Return: a number.
+ */
+void print_number(int n)
+{
+    if (n > 0) {
+        if (n > 9 && n <= 99) {
+            _putchar((n / 10) + 48);
+            _putchar((n % 10) + 48);
+        }
+        if (n > 99 && n <= 999) {
+            _putchar((n / 100) + 48);
+            _putchar((n / 10) % 10 + 48);
+            _putchar((n % 10) + 48);
+        }
+        if (n > 999 && n <= 9999) {
+            _putchar((n / 1000) + 48);
+            _putchar((n / 100) % 10 + 48);
+            _putchar((n / 10) % 10 + 48);
+            _putchar((n % 10) + 48);
+        }
+    }
+    if (n < 0) {
+        if (n < -9 && n >= -99) {
+            _putchar(45);
+            _putchar((n / 10) + 48);
+            _putchar((n % 10) + 48);
+        }
+        if (n < -99 && n <= -999) {
+            _putchar(45);
+            _putchar((n / 100) + 48);
+            _putchar((n / 10) % 10 + 48);
+            _putchar((n % 10) + 48);
+        }
+        if (n < -999 && n >= -9999) {
+            _putchar(45);
+            _putchar((n / 1000) + 48);
+            _putchar((n / 100) % 10 + 48);
+            _putchar((n / 10) % 10 + 48);
+            _putchar((n % 10) + 48);
+        }
+    }
+    if (n == 0) {
+        _putchar(48);
+    }
+}
