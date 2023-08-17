ae58681858c98395fe796467bb6155ee51d4de76
DevBasem <engr.basemahmed@gmail.com>
2023-08-17 06:26:32 -0700

    2. Collaboration is multiplicationwq

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
