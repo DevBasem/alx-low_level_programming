7ba9a6e8bd92f1a7353852c9b5712443bb3347b1
DevBasem <engr.basemahmed@gmail.com>
2023-08-24 06:51:15 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings.
+ * @dest : string 2
+ * @src : string 1
+ *
+ * Return: the two string combined
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *oldDest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		n--;
+	}
+	*dest = '\0';
+	return (oldDest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncpy - copies a string..
+ * @dest : string 2
+ * @src : string 1
+ *
+ * Return: the two string combined
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *oldDest = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		n--;
+	}
+	while (n > 0) 
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+	return (oldDest);
+}
