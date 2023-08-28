f972736f169b40788a0f422339c428229d95b010
DevBasem <engr.basemahmed@gmail.com>
2023-08-28 12:04:22 -0700

    7. The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagsums - prints the chessboard.
+ * @a: chess array.
+ * @size: chess array.
+ *
+ * Return: chessboard printed.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i == 0 || i % (size + 1) == 0)
+		{
+			sum1 += a[i];
+			j++;
+		}
+		if (i == (size - 1) || (i % (size - 1) == 0 && j < size))
+		{
+			sum2 += a[i];
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
