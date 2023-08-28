da618d1ec9d8ae53230ec72b8d26d39f233074cf
DevBasem <engr.basemahmed@gmail.com>
2023-08-27 23:17:41 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif
