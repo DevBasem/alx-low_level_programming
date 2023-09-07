b1cb095b245a1ecebdaa3c8730e287c98bc87d5e
DevBasem <engr.basemahmed@gmail.com>
2023-09-07 13:55:38 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
