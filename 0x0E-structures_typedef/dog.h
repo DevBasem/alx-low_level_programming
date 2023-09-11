4beb631f5e054face36130f780e3143152b14958
DevBasem <engr.basemahmed@gmail.com>
2023-09-11 09:49:52 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - object for dog.
+ * @name: name of the cute dog.
+ * @age: age of the cute dog.
+ * @owner: owner of the cute dog.
+ *
+ * Description: cute dog home.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
