1d1aafcb45d79a30e597e81c4ebed81df3e3271b
DevBasem <engr.basemahmed@gmail.com>
2023-09-13 14:01:37 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a call back function.
+ * @name: name of the person.
+ * @f: a function to print a name.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(f(name));
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
