eff7fe3710440b107ef1086fa71bd1ba8b857fbb
DevBasem <engr.basemahmed@gmail.com>
2023-09-21 08:21:55 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nill)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
