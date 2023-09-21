2e2679bca7ac54c1d37e8bc2e7866cdfa537213e
DevBasem <engr.basemahmed@gmail.com>
2023-09-21 09:51:18 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * pre_main - Function to print the specified text before main.
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
