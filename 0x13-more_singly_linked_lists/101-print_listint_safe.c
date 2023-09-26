eb20dbefecdcee962943bbe588a2e41da5564cda
DevBasem <engr.basemahmed@gmail.com>
2023-09-26 05:20:06 -0700

    test 1

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,49 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
+	const listint_t *walker = head, *runner = head;
 	size_t count = 0;
 
-	while (slow && fast && fast->next)
+	while (walker && runner && runner->next)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)walker, walker->n);
 		count++;
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		walker = walker->next;
+		runner = runner->next->next;
+		if (walker == runner)
 		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			exit(98);
+			walker = head;
+			while (walker != runner)
+			{
+				count++;
+				printf("[%p] %d\n", (void *)walker, walker->n);
+				walker = walker->next;
+				runner = runner->next;
+			}
+			walker = walker->next;
+			while (walker != runner)
+			{
+				count++;
+				printf("[%p] %d\n", (void *)walker, walker->n);
+				walker = walker->next;
+			}
+
+			printf("-> [%p] %d\n", (void *)walker, walker->n);
+			return (count);
 		}
 	}
-
-	while (slow)
+	while (walker)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)walker, walker->n);
 		count++;
-		slow = slow->next;
+		walker = walker->next;
 	}
-
-	return count;
+	return (count);
 }
