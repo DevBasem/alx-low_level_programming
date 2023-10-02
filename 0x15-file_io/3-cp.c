5a5f0be2415626b0246a86ab55e08a63eb9b0dcb
DevBasem <engr.basemahmed@gmail.com>
2023-10-02 08:33:10 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,81 +2,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *allocate_buffer(char *file);
-void close_descriptor(int fd);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * allocate_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file the buffer is associated with.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Returns: A pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
-char *allocate_buffer(char *file)
+char *create_buffer(char *file)
 {
-    char *buffer = malloc(1024 * sizeof(char));
+	char *buffer;
 
-    if (buffer == NULL)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't allocate buffer for %s\n", file);
-        exit(99);
-    }
-    return (buffer);
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
 }
 
 /**
- * close_descriptor - Closes a file descriptor.
+ * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
-void close_descriptor(int fd)
+void close_file(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The number of command-line arguments.
- * @argv: An array of pointers to the command-line arguments.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Returns: 0 on success.
+ * Return: 0 on success.
  *
- * Description: Exits with specific error codes for different failures:
- *   97 - Incorrect number of arguments
- *   98 - Unable to read from file_from
- *   99 - Unable to write to file_to
- *   100 - Unable to close file descriptors
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-    int source_fd, destination_fd, bytes_read;
-    char *buffer;
+	int from, to, r, w;
+	char *buffer;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp source_file destination_file\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    buffer = allocate_buffer(argv[2]);
-    source_fd = open(argv[1], O_RDONLY);
-    destination_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    while ((bytes_read = read(source_fd, buffer, 1024)) > 0)
-    {
-        if (bytes_read == -1 || write(destination_fd, buffer, bytes_read) == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: File operation failed\n");
-            free(buffer);
-            exit(99);
-        }
-    }
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
 
-    free(buffer);
-    close_descriptor(source_fd);
-    close_descriptor(destination_fd);
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 
-    return (0);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
 }
+
