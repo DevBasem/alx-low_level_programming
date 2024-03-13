f46a54975c3f4c9607ceeca53889d0c468411bde
DevBasem <engr.basemahmed@gmail.com>
2024-03-13 15:58:12 -0700

    task 5 adv

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,46 +1,110 @@
-#include "search_algos.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include "search_algos.h"
+
+void free_list(listint_t *list);
 
 /**
- * jump_list - Searches for a value in a sorted list of integers using
- *             the Jump search algorithm
+ * create_list - Creates a single linked list
+ *
+ * @array: Pointer to the array to use to fill the list
+ * @size: Size of the array
+ *
+ * Return: A pointer to the head of the created list (NULL on failure)
+ */
+listint_t *create_list(int *array, size_t size)
+{
+	listint_t *list;
+	listint_t *node;
+
+	list = NULL;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_list(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->next = list;
+		list = node;
+	}
+	return (list);
+}
+
+/**
+ * free_list - Deallocates a singly linked list
+ *
+ * @list: Pointer to the linked list to be freed
+ */
+void free_list(listint_t *list)
+{
+	listint_t *node;
+
+	if (list)
+	{
+		node = list->next;
+		free(list);
+		free_list(node);
+	}
+}
+
+/**
+ * print_list - Prints the content of a listint_t
+ *
+ * @list: Pointer to the head of the list
+ */
+void print_list(const listint_t *list)
+{
+	printf("List :\n");
+	while (list)
+	{
+		printf("Index[%lu] = [%d]\n", list->index, list->n);
+		list = list->next;
+	}
+	printf("\n");
+}
+
+/**
+ * jump_list - Searches for a value in a sorted list of integers
+ * using Jump search
+ *
  * @list: Pointer to the head of the list to search in
- * @size: Number of nodes in list
+ * @size: Number of nodes in the list
  * @value: Value to search for
  *
- * Return: Pointer to the first node where value is located,
- *         or NULL if value is not present or list is NULL
+ * Return: Pointer to the node where value is located, or NULL if not found
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	listint_t *current, *prev;
-	size_t jump = sqrt(size), i, j;
+	size_t step = sqrt(size);
+	listint_t *prev = NULL;
+	listint_t *current = NULL;
+	size_t i;
 
-	if (list == NULL)
+	if (!list || size == 0)
 		return (NULL);
 
-	current = list;
-	prev = NULL;
-
-	while (current != NULL && current->n < value)
-	{
-		prev = current;
-		for (i = 0; current->next != NULL && i < jump; i++)
+	do {
+		prev = list;
+		current = list;
+		for (i = 0; current->next && i < step; i++)
 			current = current->next;
-
-		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-	}
+		list = current;
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	} while (list->index < size - 1 && list->n < value);
 
 	printf("Value found between indexes [%lu] and [%lu]\n",
-			prev != NULL ? prev->index : 0, current->index);
+			prev->index, list->index);
 
-	for (j = prev != NULL ? prev->index : 0; j <= current->index; j++)
+	for (; prev && prev->index <= list->index; prev = prev->next)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", j, current->n);
-		if (current->n == value)
-			return (current);
-		current = current->next;
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
 	}
 
 	return (NULL);
